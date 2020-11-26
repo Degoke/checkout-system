f0b30b363c22778c5bf26355cd772d98905c0127
Degoke <69061153+Degoke@users.noreply.github.com>
2020-11-26 19:09:35 +0100

    fixed delte

--- src/components/cart-items.js
+++ src/components/cart-items.js
@@ -10,7 +10,7 @@ const CartItems = ({item}) => {
          item.price = item.fixprice * item.count
         switch(e.target.id){
             case 'add':
-            items.map((i, index) => {
+            items.forEach((i, index) => {
             if(i.id === item.id){
                 item.count ++;
                 
@@ -21,7 +21,7 @@ const CartItems = ({item}) => {
         setItems(state => [...state, {...item, price: item.fixprice * item.count}])
         break
         case 'subtract':
-        items.map((i, index) => {
+        items.forEach((i, index) => {
             if(i.id === item.id){
                 if(item.count > 1)(
                     item.count --
@@ -33,13 +33,17 @@ const CartItems = ({item}) => {
         setItems(state => [...state, {...item, price: item.fixprice * item.count}])
         break
         case 'delete':
-         items.map((i, index) => {
-            if(i.id === item.id){
-                setItems(state => state.splice(index, 1))
+         items.forEach((i, index) => {
+             setItems(state => state.filter(j => {
+                   return j.id !== item.id
+             })
+             )
                 //items.splice(index, 1);
                 sessionStorage.removeItem(i.id)
-            }
-        })
+         })
+                break
+            default:
+                setItems(state => state)
     }
     }
     return(
